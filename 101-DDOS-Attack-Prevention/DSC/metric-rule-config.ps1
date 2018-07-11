433730fef52bc17b8c7e6b4144fd870701f5d748
Aniket Gaikwad <aniket@avyanconsulting.com>
2018-07-11 15:14:31 +0530

    all paramters made mandetory

--- 101-DDOS-Attack-Prevention/DSC/metric-rule-config.ps1
+++ 101-DDOS-Attack-Prevention/DSC/metric-rule-config.ps1
@@ -1,22 +1,21 @@
 [CmdletBinding()]
 param (
-
-    # Enter AAD Username with Owner permission at subscription level and Global Administrator at AAD level.
-    [Parameter(Mandatory = $false)]
-    [Alias("user")]
-    [string]
-    $UserName = "aniket@avyanconsulting.com",
-
     # Enter the resource group name as string
-    [Parameter(Mandatory=$true)]
+    [Parameter(Mandatory = $true)]
     [Alias("rg")]
     [string]
     $ResourceGroupName,
 
     # Enter Location of ResourceGroup as string
-    [Parameter(Mandatory = $false)]
+    [Parameter(Mandatory = $true)]
     [string]
-    $Location = "East US"   
+    $Location,
+
+    # Enter emailId to get DDoS attack alert
+    [Parameter(Mandatory = $true)]
+    [Alias("user")]
+    [string]
+    $Email   
 )
 
 Write-Verbose "Configurating email alert at metrics level "
@@ -24,9 +23,9 @@ Write-Verbose "Configurating email alert at metrics level "
 $resourceId = (Get-AzureRmResource -ResourceGroupName $ResourceGroupName -ResourceType Microsoft.Network/publicIPAddresses).ResourceId
 Write-Verbose "got resourceid "
 
-$actionEmail = New-AzureRmAlertRuleEmail -CustomEmail $UserName -WarningAction SilentlyContinue
+$actionEmail = New-AzureRmAlertRuleEmail -CustomEmail $Email -WarningAction SilentlyContinue
 Write-Verbose "got action email "
 
 # Configuring the Metrics Alert rule for under DDoS attack status
-Add-AzureRmMetricAlertRule -Name "DDoS attack alert" -ResourceGroup $ResourceGroupName -location $Location -TargetResourceId $resourceId -MetricName "IfUnderDDoSAttack" -Operator GreaterThanOrEqual -Threshold 1 -WindowSize 00:05:00 -TimeAggregationOperator Total -Action $actionEmail -Description "Under DDoS attack alert" -WarningAction SilentlyContinue
+Add-AzureRmMetricAlertRule -Name "DDoS attack alert" -ResourceGroup $ResourceGroupName -location $Location -TargetResourceId $resourceId -MetricName "IfUnderDDoSAttack" -Operator GreaterThanOrEqual -Threshold 1 -WindowSize 00:05:00 -TimeAggregationOperator Total -Action $actionEmail -Description "Under DDoS attack alert"
 Write-Verbose "metric rule created"
